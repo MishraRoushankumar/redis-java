d79383be058d2dbb9621277f77d58114993ca86b
Roushankumar Mishra <mishraroushan455@gmail.com>
2026-09-10 20:01:33 +0530

    implement ping command

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -9,24 +9,21 @@ public class Main {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     System.out.println("Logs from your program will appear here!");
 
-    //  Uncomment the code below to pass the first stage
         ServerSocket serverSocket = null;
         Socket clientSocket = null;
         int port = 6379;
         try {
           serverSocket = new ServerSocket(port);
-          // Since the tester restarts your program quite often, setting SO_REUSEADDR
-          // ensures that we don't run into 'Address already in use' errors
           serverSocket.setReuseAddress(true);
-          // Wait for connection from client.
           clientSocket = serverSocket.accept();
 
           InputStream inputStream = clientSocket.getInputStream();
           byte[] buffer = new byte[1024];
-          int byteReads  = inputStream.read(buffer);
+          int byteReads;
+          while ((byteReads = inputStream.read(buffer)) != -1) {
+              clientSocket.getOutputStream().write("+PONG\r\n".getBytes());
+          }
 
-          OutputStream outputStream = clientSocket.getOutputStream();
-          outputStream.write("+PONG\r\n".getBytes());
         } catch (IOException e) {
           System.out.println("IOException: " + e.getMessage());
         } finally {
